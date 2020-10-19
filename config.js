52ed2d376c3ffc1f467c37c4dd56a611e10b154c
Rodrigo-Montenegro-Farias <59695256+Rodrigomontenegrofarias@users.noreply.github.com>
2020-10-19 14:53:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,52 +1,67 @@
 const config = {
   gatsby: {
     pathPrefix: '/',
-    siteUrl: 'https://#{site}#',
-    gaTrackingId: "#{ga}#",
+    siteUrl: 'https://hasura.io',
+    gaTrackingId: null,
     trailingSlash: false,
   },
   header: {
-    logo: 'https://www.uv.cl/images/menu_marca_mono.png',
-    logoLink: 'https://#{site}#',
+    logo: 'https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/brand.svg',
+    logoLink: 'https://hasura.io/learn/',
     title:
-      "Documentos de clases de Desarrollo Web y Arquitectura Web",
+      "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
     githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
     helpUrl: '',
     tweetText: '',
-    social: ``,
+    social: `<li>
+		    <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>
+			<li>
+		    <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+		      <div class="discordBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>`,
     links: [{ text: '', link: '' }],
     search: {
-      enabled: true,
-      indexName: '#{indexName}#',
-      algoliaAppId: "#{algoliaAppId}#",
-      algoliaSearchKey: "#{algoliaSearchKey}#",
-      algoliaAdminKey: "#{algoliaAdminKey}#",
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
     },
   },
   sidebar: {
     forcedNavOrder: [
+      '/introduction', // add trailing slash if enabled above
+      '/codeblock',
     ],
     collapsedNav: [
-      
+      '/codeblock', // add trailing slash if enabled above
     ],
-    links: [{ text: 'trifenix', link: 'https://www.trifenix.io' }],
-    frontline: true,
+    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    frontline: false,
     ignoreIndex: true,
     title:
-      "Docs",
+      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
   },
   siteMetadata: {
-    title: 'Arquitectura de software - Desarrollo Web',
-    description: 'Cursos de la universidad de valparaiso',
+    title: 'Gatsby Gitbook Boilerplate | Hasura',
+    description: 'Documentation built with mdx. Powering hasura.io/learn ',
     ogImage: null,
-    docsLocation: '#{home-git}#',
-    favicon: 'https://www.uv.cl/favicon.ico',
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
   },
   pwa: {
-    enabled: true, // disabling this will also remove the existing service worker.
+    enabled: false, // disabling this will also remove the existing service worker.
     manifest: {
-      name: 'Docs de la universidad de valparaiso',
-      short_name: 'valpo-docs',
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
       start_url: '/',
       background_color: '#6b37bf',
       theme_color: '#6b37bf',
